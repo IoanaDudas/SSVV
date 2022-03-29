cf9171b6fe92fffc9f090d1f04c5bcc84733935a
IoanaDudas <72069893+IoanaDudas@users.noreply.github.com>
2022-03-29 13:59:30 +0300

    -- added more tests for TestSaveAssignment class

--- src/test/java/TestSaveAssignment.java
+++ src/test/java/TestSaveAssignment.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestSaveAssignment {
     @Test
     public void TestSaveAssignment_DataIsValidAndNonexistent_Success() {
-        Tema tema = new Tema("5", "tema", 10, 5);
+        Tema tema = new Tema("5", "tema", 10, 8);
         Validator<Tema> temaValidator = new TemaValidator();
         TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "test_teme.xml");
 
@@ -21,6 +21,42 @@ public class TestSaveAssignment {
     @Test
     public void TestSaveAssignment_DataIsValidAndExistent_ReturnsNull() {
         Tema tema = new Tema("2", "XML", 8, 7);
+        Validator<Tema> temaValidator = new TemaValidator();
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "test_teme.xml");
+
+        assertNull(temaXMLRepository.save(tema));
+    }
+
+    @Test
+    public void TestSaveAssignment_IdIsNull_ReturnsNull() {
+        Tema tema = new Tema(null, "tema", 10, 8);
+        Validator<Tema> temaValidator = new TemaValidator();
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "test_teme.xml");
+
+        assertNull(temaXMLRepository.save(tema));
+    }
+
+    @Test
+    public void TestSaveAssignment_DescriptionIsEmpty_ReturnsNull() {
+        Tema tema = new Tema("5", "", 10, 8);
+        Validator<Tema> temaValidator = new TemaValidator();
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "test_teme.xml");
+
+        assertNull(temaXMLRepository.save(tema));
+    }
+
+    @Test
+    public void TestSaveAssignment_DeadlineIsGreaterThan14_ReturnsNull() {
+        Tema tema = new Tema("5", "tema", 15, 8);
+        Validator<Tema> temaValidator = new TemaValidator();
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "test_teme.xml");
+
+        assertNull(temaXMLRepository.save(tema));
+    }
+
+    @Test
+    public void TestSaveAssignment_StartineIsSmallerThan1_ReturnsNull() {
+        Tema tema = new Tema("5", "tema", 10, 0);
         Validator<Tema> temaValidator = new TemaValidator();
         TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "test_teme.xml");
 
